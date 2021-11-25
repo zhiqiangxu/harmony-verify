606c3791c17996c76eda0bc4669c7f0447d68fdd
徐志强 <652732310@qq.com>
2021-11-25 13:45:24 +0800

    call GetBlockByNumber

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ import (
 const node = "https://api.s1.b.hmny.io"
 
 func main() {
-	result, err := request(rpc.Method.GetLatestBlockHeader, []interface{}{})
-	if err != nil {
-		panic(err)
+	{
+		result, err := request(rpc.Method.GetLatestBlockHeader, []interface{}{})
+		if err != nil {
+			panic(err)
+		}
+
+		asJSON, _ := json.Marshal(result)
+		fmt.Println(common.JSONPrettyFormat(string(asJSON)))
 	}
 
-	asJSON, _ := json.Marshal(result)
-	fmt.Println(common.JSONPrettyFormat(string(asJSON)))
+	{
+		result, err := request(rpc.Method.GetBlockByNumber, []interface{}{int64(10)})
+		if err != nil {
+			panic(err)
+		}
+
+		asJSON, _ := json.Marshal(result)
+		fmt.Println(common.JSONPrettyFormat(string(asJSON)))
+	}
 }
